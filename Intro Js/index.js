99a09e2c5b737f0c30315fd1502810c94849c1a0
Ultimate <81652786+ultimate-star@users.noreply.github.com>
2022-06-23 15:49:39 +0100

    Update index.js

--- Intro Js/index.js
+++ Intro Js/index.js
@@ -1,8 +1,6 @@
 //decleration 
 		let fullName = document.getElementById('nameValue');
 		fullName.innerHTML = "Olayiwola Damilare"
-		let age = document.getElementById('ageValue');
-		age.innerHTML = "21yrs"
 		let height = document.getElementById('heightValue');
 		height.innerHTML = "5.4 ft"
 		let country = document.getElementById('countryValue');
